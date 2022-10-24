67d03603802027bb573f37f79025b568ba92366a
Diana Rojas <dianarojas@Dianas-iMac.broadband>
2022-10-24 13:05:05 +0100

    Initial commit

---
+++ updatesservice/src/main/java/com/example/preassignment/updatesservice/UpdatesserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.preassignment.updatesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdatesserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdatesserviceApplication.class, args);
+	}
+
+}
